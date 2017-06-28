ae2c7be5fb9b0475480334fde2f53049f3fe47e9
KIRUTHIKA555 <kiruthikakt38@gmail.com>
2017-06-28 14:51:59 +0530

    Update and rename D.C to kriti.C

---
+++ kriti.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int n;
+  clrscr():
+  printf("\n enter the numbers");
+  scanf("%d",&n);
+  if(n<0)
+  {
+    printf("\n the given number is positive");
+  }
+  if(n>0)
+  {
+    printf("\n the given number is negative");
+  }
+  if(n==0)
+  {
+    printf("\n the given number is zero");
+  }
+  getch();
+}
+ 
+  
